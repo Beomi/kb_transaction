e9e745e71399cec89f8a04eb6a20ee6e22048d3e
Beomi <latheledusjp@gmail.com>
2017-09-06 02:13:04 +0900

    Update to 0.0.2 for PYPI :(

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='kb_transaction',
-      version='0.0.1',
+      version='0.0.2',
       url='https://github.com/beomi/kb-transaction',
       license='MIT',
       author='Junbum Lee',
@@ -11,5 +11,5 @@ setup(name='kb_transaction',
       long_description=open('README.md').read(),
       zip_safe=False,
       setup_requires=['python-dateutil', 'beautifulsoup4', 'selenium'],
-      download_url='https://github.com/beomi/kb_transaction/archive/0.0.1.tar.gz',
+      download_url='https://github.com/beomi/kb_transaction/archive/0.0.2.tar.gz',
       )
